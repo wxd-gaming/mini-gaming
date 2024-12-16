7bac3ef0cd6aeeb38a16caf5e6adc67c9172f415
wxd-gaming <492794628@qq.com>
2024-12-16 15:48:37 +0800

    初始化

---
+++ mini-server/src/main/java/wxdgaming/spring/minigame/module/data/ServerDataCenter.java
@@ -0,0 +1,10 @@
+package wxdgaming.spring.minigame.module.data;
+
+/**
+ * 区服数据
+ *
+ * @author: wxd-gaming(無心道, 15388152619)
+ * @version: 2024-12-16 15:44
+ **/
+public class ServerDataCenter {
+}
